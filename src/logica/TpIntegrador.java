cb3955d15ef6c9ae2e822829a103e4f26713cc0f
germanflores1995 <germanflores1995@gmail.com>
2024-12-05 03:33:11 -0300

    Repositorio de sorteador

---
+++ src/logica/TpIntegrador.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package logica;
+
+import igu.Principal;
+
+/**
+ *
+ * @author Germán
+ */
+public class TpIntegrador {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal ventana = new Principal();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
+    }
+    
+}
